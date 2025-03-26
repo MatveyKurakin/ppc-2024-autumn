aa3c149f2ebdbbd9bacfd7dcf1e03c5771efb718
Matvej Kurakin <matvejkurakin7755@gmail.com>
2025-03-26 22:40:03 +0300

    change perf tests

--- tasks/omp/example/perf_tests/main.cpp
+++ tasks/omp/example/perf_tests/main.cpp
@@ -7,7 +7,7 @@
 #include "core/perf/include/perf.hpp"
 #include "omp/example/include/ops_omp.hpp"
 
-TEST(openmp_example_perf_test, test_task_run) {
+TEST(openmp_example_perf_test_const, test_openmp_task_run) {
   kurakin_m_monte_carlo_omp::Integral integral{
       .func_ = [](std::vector<double> x) { return x[0]; }, .bounds_ = {{-1, 1}}, .iterations_ = 1000000};
   std::vector<double> res(1, 0);
@@ -37,7 +37,7 @@ TEST(openmp_example_perf_test, test_task_run) {
   ASSERT_NEAR(0, res[0], 0.01);
 }
 
-TEST(sequential_example_perf_test, test_task_run) {
+TEST(openmp_example_perf_test_const, test_seq_task_run) {
   kurakin_m_monte_carlo_omp::Integral integral{
       .func_ = [](std::vector<double> x) { return x[0]; }, .bounds_ = {{-1, 1}}, .iterations_ = 1000000};
   std::vector<double> res(1, 0);
@@ -70,6 +70,73 @@ TEST(sequential_example_perf_test, test_task_run) {
   perfAnalyzer->task_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
   ASSERT_NEAR(0, res[0], 0.01);
+}
+
+TEST(openmp_example_perf_test_dimension_1, test_openmp_task_run) {
+  kurakin_m_monte_carlo_omp::Integral integral{.func_ = [](std::vector<double> x) { return x[0] + x[1] + x[2]; },
+                                               .bounds_ = {{-1, 1}, {-1, 1}, {-1, 1}},
+                                               .iterations_ = 1000000};
+  std::vector<double> res(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataOMP = std::make_shared<ppc::core::TaskData>();
+  taskDataOMP->inputs.emplace_back(reinterpret_cast<uint8_t *>(&integral));
+  taskDataOMP->inputs_count.emplace_back(size_t(1));
+  taskDataOMP->outputs.emplace_back(reinterpret_cast<uint8_t *>(res.data()));
+  taskDataOMP->outputs_count.emplace_back(res.size());
+
+  // Create Task
+  auto testTaskOMP = std::make_shared<kurakin_m_monte_carlo_omp::TestOMPTaskParallel>(taskDataOMP);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  perfAttr->current_timer = [&] { return omp_get_wtime(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskOMP);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  ASSERT_NEAR(0, res[0], 0.1);
+}
+
+TEST(openmp_example_perf_test_dimension_1, test_seq_task_run) {
+  kurakin_m_monte_carlo_omp::Integral integral{.func_ = [](std::vector<double> x) { return x[0] + x[1] + x[2]; },
+                                               .bounds_ = {{-1, 1}, {-1, 1}, {-1, 1}},
+                                               .iterations_ = 1000000};
+  std::vector<double> res(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&integral));
+  taskDataSeq->inputs_count.emplace_back(size_t(1));
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(res.data()));
+  taskDataSeq->outputs_count.emplace_back(res.size());
+
+  // Create Task
+  auto testTaskSeq = std::make_shared<kurakin_m_monte_carlo_omp::TestTaskSequential>(taskDataSeq);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSeq);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  ASSERT_NEAR(0, res[0], 0.1);
 }
 
 int main(int argc, char **argv) {
